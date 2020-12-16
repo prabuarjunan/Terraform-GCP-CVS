b2a77303fd137ecd32eff389dc70547bbac7e4a3
prabu <prabuarjunan@gmail.com>
2020-12-16 15:12:37 -0800

    commit terraform

--- CVS_GCP.tf
+++ CVS_GCP.tf
@@ -7,7 +7,7 @@ resource "netapp-gcp_volume" "gcp-volume" {
   protocol_types = ["NFSv3"]
   network = "default"
   size = 1024
-  service_level = "basic"
+  service_level = "premium"
   volume_path = "deleteme_asap"
   snapshot_policy {
     enabled = true
@@ -40,30 +40,4 @@ resource "netapp-gcp_volume" "gcp-volume" {
   }
 }
 
-# Create a snapshot tied to an volume
-resource "netapp-gcp_snapshot" "gcp-snapshot" {
-  name = "deleteme_asapGO_jusitin-snapshot"
-  region = "us-west2"
-  volume_name =  "deleteme_asapGO_jusitin"
-  # creation_token = "unique-token-number"
-}
-
-# Create an Active Directory
-resource "netapp-gcp_active_directory" "gcp-active-directory" {
-  provider = netapp-gcp
-  region = "us-west2"
-    username = "test_user"
-    password = "netapp"
-  domain = "example.com"
-  dns_server = "10.0.0.0"
-  net_bios = "cvsserver"
-}
-
-# Create a volume_backup tied to an volume
-resource "netapp-gcp_volume_backup" "gcp-volume-backup" {
-  name = "main-volume-backup"
-  region = "us-west2"
-  volume_name =  "main-volume"
-  creation_token = "unique-token-number"
-}
 #
