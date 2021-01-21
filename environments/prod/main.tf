51c933c3fadaca6cc1a553401d8baa955c88f8c0
prabu <prabuarjunan@gmail.com>
2021-01-21 09:10:12 -0800

    commit terraform

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,5 +1,7 @@
 // Configure the Google Cloud provider
 provider "google" {
+ #credentials = file("/Users/arjunan/Downloads/cloud-heroes-c03315584bc8.json")
+ #project     = "cloud-heroes"
  credentials = file(var.credentials_file)
  project = var.project
  region      = "us-central1"
